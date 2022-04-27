7276eecad4a1f91820d5b8d4b0cc99ad14ad99ea
Juan Pablo Correa <juanpablocorreapuerta@gmail.com>
2022-04-27 15:19:25 -0500

    Tarea initialization

---
+++ vertex_cover_1.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+graph = {
+    0: [3, 4],
+    1: [2, 5],
+    2: [1, 3],
+    3: [0, 2, 4, 5],
+    4: [0, 3, 5],
+    5: [1, 3, 4]
+}
+
+
+def vertex_cover_aa1(graph):
+    eje = graph.get('0')
+
+
+    return True
+
+
+vertex_cover_aa1(graph)
