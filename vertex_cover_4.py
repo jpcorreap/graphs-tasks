14c9096926b26e0b485c73a08333038fdfe699a0
Sergio Sierra <sd.sierra@uniandes.edu.co>
2022-05-23 21:57:24 -0500

    Finished 4, fixed 3

--- vertex_cover_4.py
+++ vertex_cover_4.py
@@ -1,36 +1,35 @@
 from graph_utils import remove_all_edges
-import random
+from random import choice, sample
 
 
-def vertex_cover_ra4(graph):
-    # Variables
-    rta = []
-    vertices = []
-    for i in range(len(graph)):
-        vertices.append(i)
+def vertex_cover_ra4(graph: dict) -> tuple:
+    vertex_cover = set()
+    finished, origin, destiny = _get_random_edge(graph)
+    while finished:
+        picked_vertex = choice([origin, destiny])
+        graph = remove_all_edges(graph, picked_vertex)
+        vertex_cover.add(picked_vertex)
+        finished, origin, destiny = _get_random_edge(graph)
+    return vertex_cover, len(vertex_cover)
 
-    while(len(vertices) != 0):
-        # Escoger aleatoriamente un eje
-        verticeOrigen = random.choice(vertices)
-        while len(graph[verticeOrigen]) == 0:
-            vertices.remove(verticeOrigen)
-            if len(vertices) == 0:
-                return rta
-            verticeOrigen = random.choice(vertices)
-        verticeDestino = random.choice(graph[verticeOrigen])
-        print("Eje escogido: {}-{}".format(verticeOrigen, verticeDestino))
 
-        # Escoger aletoriamente un vertice
-        binario = random.randint(0, 1)
-        if binario == 0:
-            vertice = verticeOrigen
+def _get_random_edge(graph: dict) -> tuple:
+    """
+        Selecciona aleatoriamente un eje
+        Si no quedan ejes para escoger, retorna False, None, None
+    """
+    found = False
+    random_origin_vertex = None
+    random_destiny_vertex = None
+    all_processed = set(graph.keys())
+    while not found and len(all_processed) > 0:
+        if len(graph.keys()) > 0:
+            random_origin_vertex = sample(all_processed, 1)[0]
+            all_processed.remove(random_origin_vertex)
+            if len(graph[random_origin_vertex]) > 0:
+                random_destiny_vertex = sample(
+                    graph[random_origin_vertex], 1)[0]
+                found = True
         else:
-            vertice = verticeDestino
-        print("Vertice escogido:", vertice)
-        rta.append(vertice)
-        vertices.remove(vertice)
-
-        # Descartar ejes del vertice
-        graph = remove_all_edges(graph, vertice)
-
-    return rta
+            found = False
+    return found, random_origin_vertex, random_destiny_vertex
