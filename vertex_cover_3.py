138ae6e6f38798c4d307e4a3969e2d2adcf1c4b2
Sergio Sierra <sd.sierra@uniandes.edu.co>
2022-05-23 21:00:51 -0500

    Fix aa3

--- vertex_cover_3.py
+++ vertex_cover_3.py
@@ -5,17 +5,15 @@ def vertex_cover_aa3(graph: dict) -> set:
     V = len(graph)
     vertex_cover = set()
     for vertex in range(V):
-        if len(graph[vertex]) > 1:
-            for vertex_2 in graph[vertex]:
-                break
-            max_vertex = -1
-            if len(graph[vertex]) > len(graph[vertex_2]):
-                max_vertex = vertex
-            else:
-                max_vertex = vertex_2
-            graph = remove_all_edges(graph, max_vertex)
-            vertex_cover.add(max_vertex)
+        if vertex in graph:
+            if len(graph[vertex]) > 1:
+                for vertex_2 in graph[vertex]:
+                    break
+                max_vertex = -1
+                if len(graph[vertex]) > len(graph[vertex_2]):
+                    max_vertex = vertex
+                else:
+                    max_vertex = vertex_2
+                graph = remove_all_edges(graph, max_vertex)
+                vertex_cover.add(max_vertex)
     return vertex_cover, len(vertex_cover)
-
-from graph_utils import read_graph
-print(vertex_cover_aa3(read_graph("ensayo.txt")))
